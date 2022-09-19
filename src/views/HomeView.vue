0ea2c66492580628ddf1726c437962c99eadc89a
unknown <admin@sh-fm-ws-specpo.nis.edu.kz>
2022-09-19 16:56:30 +0600

    all

--- src/views/HomeView.vue
+++ src/views/HomeView.vue
@@ -1,6 +1,5 @@
 <template>
-    <div class="wrapper">
-
+    <div class="wrapper_big">
         <div class="top__img" style="margin-top: -5px">
             <Icon name="main-school" alt="image" style="margin-bottom: 20px;" />
             <div class="content-head">
@@ -8,6 +7,16 @@
             </div>
             <div class="content-body">
                 <p>Способствовать повышению качества Казахстанского образования через воспитание интеллектуального и физически развитого гражданина мира с высокими моральными ценностями</p>
+            </div>
+        </div>
+    </div>
+    <div class="wrapper">
+        <!--Vidieos start-->
+        <div class="videos">
+            <div class="videos__wrapper">
+                <div class="video__item">
+                    <iframe class="video__element" src="https://www.youtube.com/embed/vtYM2dy22lM" title="YouTube video player" frameborder="0" allow="accelerometer; autoplay; clipboard-write; encrypted-media; gyroscope; picture-in-picture" allowfullscreen></iframe>
+                </div>
             </div>
         </div>
 
@@ -40,7 +49,7 @@ export default {
     .top__img img {
         vertical-align: middle;
         width: 100%;
-        max-width: 1200px;
+        max-width: 1440px;
         max-height: 600px;
         object-position: 10% 10%;
         object-fit: cover;
@@ -66,5 +75,24 @@ export default {
         width: 50%;
         padding: 20px;
         font-weight: 500;
+    }
+    .videos{
+        width: 100%;
+    }
+    .videos__wrapper{
+        max-width: 1200px;
+        margin: 0 auto;
+        position: relative;
+    }
+    .video__item{
+        padding-top: 56%;
+    }
+    .video__element{
+        position: absolute;
+        top: 0;
+        left: 0;
+        width: 100%;
+        height: 100%;
+        border: 0;
     }
 </style>
